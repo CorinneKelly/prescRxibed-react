21cf2948a9a031f86c5cdd020967d607dab43786
rmody3 <rmody3@gmail.com>
2017-04-17 20:21:31 -0400

    added axios calls to api and googlelogin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login'
+import axios from 'axios'
 import App from './App';
 import './index.css';
 
+
+const responseGoogle = (response) => {
+  debugger
+  axios
+  .post('http://localhost:4000/v1/sessions', {account: {googleToken: response.accessToken, googleId: response.googleId}})
+  .then(function(response){
+    console.log('logged in successfully')
+    console.log(response)
+  });
+}
+
+
+
+
 ReactDOM.render(
-  <App />,
+  <div>
+    <App />
+    <GoogleLogin
+      //clientId="" add it from evernote
+      
+      buttonText = "Login with Google"
+      responseType
+      onSuccess = {responseGoogle}
+      onFailure = {responseGoogle}
+     />
+  </div>,
   document.getElementById('root')
 );
