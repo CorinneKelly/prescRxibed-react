5521246b2a3578db8c1db4338a5f144b6110a2a8
CorinneKelly <corinnemariekelly@gmail.com>
2017-04-19 22:13:02 -0400

    Edited AddPrescription Component form to update all states on change, Fixed, burgermenu CSS animation to get push effect working

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,9 +3,8 @@ import React, {Component} from 'react'
 class MenuItem extends Component {
 	render() {
 		return (
-
-			<ul>
-				<li><a href="/logout">Log Out</a></li>
+			<ul className="burger-menu-list" >
+				<li><a href="/logout" >Log Out</a></li>
 				<li><a href="/">Home</a></li>
 			</ul>
 		)
